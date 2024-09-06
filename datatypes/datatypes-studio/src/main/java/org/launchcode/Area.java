6f235650bf1d34a29ac9c6364af35823cef113fb
KevinAd92 <105957644+KevinAd92@users.noreply.github.com>
2024-09-05 19:56:25 -0500

    studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,6 +6,13 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("enter a radius?");
         Double radius = input.nextDouble();
+       // if ( radius == ){
+       // }
+        while (radius <= 0) {
+            System.out.println("Incorrect value with a radius of " + radius + " an area cannot be found.");
+            System.out.println("Enter a radius");
+            radius = input.nextDouble();
+        }
 
         Double area = Circle.getArea(radius);
         System.out.print("The area of a circle with a radius of "+ radius +" is: " + area);
